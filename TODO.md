f3f0affa239df10fdd72a3ca736c7ac46f51d7cb
Pelemenguin <elepentuin@outlook.com>
2025-05-24 14:55:28 +0800

    Add `coal` but not its trait yet

--- TODO.md
+++ TODO.md
@@ -4,6 +4,7 @@
 - 添加 `calcite`，`tuff`，`prismarine`，`dripstone` 作为 `rock` 变种。
 ## 计划创造材料
 - 添加 `soul` 作为 `glass` 的变种。
+- 添加材料 `coal`。
 ## 计划创建配方
 - 添加材料 `glass#*_stained` 配方。
 ## 杂项
---
+++ kubejs/startup_scripts/events/on_living_attack.js
@@ -0,0 +1,46 @@
+ForgeEvents.onEvent("net.minecraftforge.event.entity.living.LivingAttackEvent", event => {
+
+    console.info(`[LivingHurtEvent] Triggered by ${event.entity}`)
+    try {
+        let mainhandItem = (event.source.actual.handSlots[0])
+        let offhandItem = (event.source.actual.handSlots[1])
+
+        if (mainhandItem.nbt != null) {
+            if (mainhandItem.nbt.get("tic_broken").asInt != 1) {
+                let attacker_weapon_modifier_data = mainhandItem.nbt.get("tic_modifiers")
+                var attacker_weapon_modifiers = {}
+                attacker_weapon_modifier_data.forEach(modifier => {
+                    let name = modifier.get("name").asString
+                    let level = modifier.get("level").asInt
+                    attacker_weapon_modifiers[name] = level
+                })
+
+                run_modifiers(event, mainhandItem, attacker_weapon_modifiers)
+            }
+        }
+
+        if (offhandItem.nbt != null) {
+        if (offhandItem.nbt.get("tic_broken").asInt != 1) {
+            let attacker_offhand_weapon_modifier_data = offhandItem.nbt.get("tic_modifiers")
+            let attacker_offhand_weapon_modifiers = {}
+            let valid_offhand = false
+            attacker_offhand_weapon_modifier_data.forEach(modifier => {
+                let name = modifier.get("name").asString
+                let level = modifier.get("level").asInt
+                attacker_offhand_weapon_modifiers[name] = level
+                if (OFFHAND_ATTACKABLE_MODIFIER.indexOf(name) != -1) {
+                    valid_offhand = true
+                }
+            })
+            console.info(valid_offhand)
+            if (valid_offhand) {
+                run_modifiers(event, offhandItem, attacker_offhand_weapon_modifiers)
+            }
+        }
+    }
+    } catch (e) {
+        console.error("Error occured!")
+        console.error(e)
+    }
+
+})
