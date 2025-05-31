a21cc1b10fc5a830460df4f133045c735ca45113
Pelemenguin <elepentuin@outlook.com>
2025-05-31 23:24:35 +0800

    Fix: `igniting` make sound even when not on fire

--- kubejs/server_scripts/events/on_entity_hurt.js
+++ kubejs/server_scripts/events/on_entity_hurt.js
@@ -1,6 +1,7 @@
 // import { getModifiersFromItem } from "../../startup_scripts/globals";
 import { relaying } from "../modifiers/relaying"
 import { glass_shard } from "../modifiers/glass_shard"
+import { igniting_trigger } from "../modifiers/igniting"
 
 let getModifiersFromItem = global.getModifiersFromItem
 
@@ -71,49 +72,6 @@ function run_modifiers(event, item, attacker_weapon_modifiers) {
     // Igniting
     if ("kubejs:igniting" in attacker_weapon_modifiers) {
         igniting_trigger(item, event.entity, attacker_weapon_modifiers["kubejs:igniting"])
-    }
-
-}
-
-const IGNITING_FIRE_PERCENTAGE_PER_LEVEL = 0.25
-
-/**
- * 
- * @param {Internal.ItemStack} item 
- * @param {Internal.LivingEntity} target 
- * @param {int} level 
- */
-function igniting_trigger(item, target, level) {
-
-    // Get or set fire time
-    console.info("[Igniting] Triggered!")
-    /** @type {Internal.CompoundTag} */
-    let persistent_data = NBT.compoundTag()
-    try {
-        persistent_data = item.nbt.get("tic_persistent")
-    } catch (e) {}
-    if (!persistent_data.contains("kubejs:igniting")) {
-        persistent_data.put("kubejs:igniting", NBT.compoundTag())
-    }
-    /** @type {Internal.CompoundTag} */
-    let trait_tag = persistent_data.get("kubejs:igniting")
-    let fire_time = target.getRemainingFireTicks()
-    if (!trait_tag.contains("fire")) {
-        trait_tag.putInt("fire", 0)
-        console.info(`[Igniting] "fire" tag does not exist. Created.`)
-    }
-    let tool_fire_time = trait_tag.get("fire")
-    console.info(`[Igniting] Tool's fire time is ${tool_fire_time}`)
-    console.info(`[Igniting] Target's fire time is ${fire_time}`)
-    if (tool_fire_time > fire_time) {
-        let applied_time = tool_fire_time * IGNITING_FIRE_PERCENTAGE_PER_LEVEL * level
-        console.info(`[Igniting] Set target's fire time to ${applied_time}`)
-        target.remainingFireTicks = JavaMath.round(applied_time)
-        target.playSound("entity.generic.burn")
-    } else {
-        console.info(`[Igniting] Set tool's fire time to ${fire_time}`)
-        trait_tag.putInt("fire", fire_time)
-        target.playSound("entity.generic.burn")
     }
 
 }
--- kubejs/server_scripts/events/on_item_tick.js
+++ kubejs/server_scripts/events/on_item_tick.js
@@ -2,6 +2,7 @@
 
 // import { getModifiersFromItem } from "../../startup_scripts/globals"
 import { incompact } from "../modifiers/incompact"
+// import { igniting_tick } from "../modifiers/igniting"
 
 let getModifiersFromItem = global.getModifiersFromItem
 
@@ -35,37 +36,4 @@ function process_item(event, item) {
         igniting_tick(item, event.player)
     }
 
-}
-
-/**
- * 
- * @param {Internal.ItemStack} item 
- * @param {Internal.Player} player 
- */
-function igniting_tick(item, player) {
-    
-    if (item.nbt.get("tic_broken").asInt == 1) {
-        console.info("[Igniting] Tool has already broken")
-        return
-    }
-
-    /** @type {Internal.CompoundTag} */
-    let persistent_data = item.nbt.get("tic_persistent")
-    if (persistent_data == null) {return}
-    /** @type {Internal.CompoundTag} */
-    let trait_data = persistent_data.get("kubejs:igniting")
-    if (trait_data == null) {return}
-    /** @type {int} */
-    let tool_fire_time = trait_data.get("fire")
-    if (tool_fire_time == null) {return}
-    if (tool_fire_time > 0) {
-        tool_fire_time -= 1
-        trait_data.putInt("fire", tool_fire_time)
-        if (tool_fire_time % 5 == 0) {
-            if (!player.creative) {
-                item.damageValue += 1
-            }
-            player.playSound(`block.furnace.fire_crackle`)
-        }
-    }
 }
