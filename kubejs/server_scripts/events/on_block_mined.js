77c190f53a72ddd1026e78af7f4048ad625afc78
Pelemenguin <elepentuin@outlook.com>
2025-06-09 22:32:29 +0800

    Fix: `BlockEvents.broken` run twice

--- kubejs/server_scripts/events/on_block_mined.js
+++ kubejs/server_scripts/events/on_block_mined.js
@@ -2,12 +2,25 @@ import { uncertain } from "../modifiers/uncertain";
 
 var getModifiersFromItem = global.getModifiersFromItem
 
+var lastBlockPos = new BlockPos(new Vec3i(0, -32767, 0))
+var lastBrokenTime = -1
+
 BlockEvents.broken(event => {
+
+    // 防止执行两次
+    let thisBrokenTime = event.level.time
+    let thisBlockPos = event.getBlock().getPos()
+    if (thisBlockPos == lastBlockPos && thisBrokenTime == lastBrokenTime) {return}
+    lastBlockPos = thisBlockPos
+    lastBrokenTime = thisBrokenTime
+    
+    console.info('A block is mined: '+lastBlockPos)
     let entity = event.entity
     if (entity == null) {return}
     let mainhandItem = entity.handSlots[0]
 
     if (mainhandItem != null && mainhandItem.nbt != null) {
+        console.info('Before calling')
         process_item_on_mine(event, mainhandItem)
     }
 })
@@ -18,7 +31,8 @@ BlockEvents.broken(event => {
  * @param {Internal.ItemStack} item 
  */
 function process_item_on_mine(event, item) {
-
+    
+    console.info('Function called!')
     let modifier_data = item.nbt.get("tic_modifiers")
     if (modifier_data == null) {return}
     var modifiers = getModifiersFromItem(item)
