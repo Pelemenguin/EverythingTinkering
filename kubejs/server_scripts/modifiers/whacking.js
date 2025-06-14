8bfb6272419417cb6c3940ebecbdc42b8a23a78c
Pelemenguin <elepentuin@outlook.com>
2025-06-14 21:54:37 +0800

    Rewrite `whacking` to let it perform like before

--- kubejs/server_scripts/modifiers/whacking.js
+++ kubejs/server_scripts/modifiers/whacking.js
@@ -1,38 +1,19 @@
 /**
  * Whacking - Multiplies damage when falling, consumes 25% of remaining durability
  * 
- * @param {Internal.LivingHurtEvent} event 
+ * @param {Internal.CriticalHitEvent} event 
+ * @param {Internal.ItemStack} item 
  * @param {boolean} isCritical 
  * @param {int} level 
  */
-function whacking(event, isCritical, level) {
-    // let source = event.source
-
-    // /** @type {Internal.ServerPlayer} */
-    // let attacker = source.actual
-
-    // // Check if crit
-    // if (attacker && attacker.isPlayer()) {
-        // if (
-            // !attacker.onGround() &&
-            // !attacker.onClimbable() &&
-            // !attacker.isInWater() &&
-            // !attacker.isInLava() &&
-            // !attacker.isPassenger() &&
-            // !attacker.isSprinting() &&
-            // attacker.fallDistance > 0
-        // ) {
-            // event.amount *= 1.25 + level * 0.25
-
-            // // Damage item
-            // if (!attacker.creative) {
-                // let remaining_dura = item.maxDamage - item.damageValue
-                // let consuming_dura = JavaMath.max(JavaMath.ceil(remaining_dura * 0.25), 10)
-                // item.damageValue += consuming_dura
-            // }
-        // }
-    // 
+function whacking(event, item, isCritical, level) {
     if (isCritical) {
-        event.setDamageModifier(event.getDamageModifier() + level * 0.5)
+        event.setDamageModifier(event.damageModifier + level * 0.5)
+        // Damage item
+        if (!event.entity.creative) {
+            let remaining_dura = item.maxDamage - item.damageValue
+            let consuming_dura = JavaMath.max(JavaMath.ceil(remaining_dura * 0.25), 10)
+            item.damageValue += consuming_dura
+        }
     }
 }
