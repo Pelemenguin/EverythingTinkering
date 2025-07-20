07209219ddcb4c8aa61642a74c040666ca3d6703
Pelemenguin <elepentuin@outlook.com>
2025-07-20 22:35:51 +0800

    Temporary commit.
    - Tried to create fluids for `sea_alloy`.

--- kubejs/startup_scripts/constants.js
+++ kubejs/startup_scripts/constants.js
@@ -1,1 +1,16 @@
-const LivingEntity = Java.loadClass("net.minecraft.world.entity.LivingEntity")
+const LivingEntity = Java.loadClass("net.minecraft.world.entity.LivingEntity");
+const MapColor = Java.loadClass("net.minecraft.world.level.material.MapColor");
+const BlockPathTypes = Java.loadClass("net.minecraft.world.level.pathfinder.BlockPathTypes")
+const SoundEvents = Java.loadClass("net.minecraft.sounds.SoundEvents");
+
+const FluidType = Java.loadClass("net.minecraftforge.fluids.FluidType");
+const FluidType$Properties = Java.loadClass("net.minecraftforge.fluids.FluidType$Properties");
+const ForgeFlowingFluid = Java.loadClass("net.minecraftforge.fluids.ForgeFlowingFluid");
+const SoundActions = Java.loadClass("net.minecraftforge.common.SoundActions")
+
+const FluidDeferredRegister = Java.loadClass("slimeknights.mantle.registration.deferred.FluidDeferredRegister");
+const FlowingFluidObject = Java.loadClass("slimeknights.mantle.registration.object.FlowingFluidObject");
+
+const BurningLiquidBlock = Java.loadClass("slimeknights.tconstruct.fluids.block.BurningLiquidBlock");
+
+const FLUIDS = new FluidDeferredRegister("kubejs");
---
+++ kubejs/startup_scripts/registries/fluid.js
@@ -0,0 +1,70 @@
+/**
+ * @fileoverview Fluid registries | 流体注册
+ * 
+ * @author Pelemenguin
+ * @license CC-BY-NC-SA-4.0
+ */
+
+/**
+ * An interface contains all fluids registered by this modpack.  
+ * 包含所有由该整合包注册的流体的接口。
+ * - - - - -
+ * @class
+ * @interface
+ */
+function KubeJSFluids() { }
+
+/**
+ * A fluid preset: hot / molten fluids  
+ * 流体预设：热 / 熔融液体
+ * 
+ * Referred to Tinker's Construct's source code
+ * - - - - -
+ * @param {Registry.Fluid} event
+ * The fluid registry event.
+ * 流体注册事件。
+ * - - - - -
+ * @param {String} name
+ * Id of the fluid.  
+ * 液体 ID。
+ * - - - - -
+ * @returns {Internal.FluidType$Properties}
+ */
+let presetHot = (name) => {
+    return FluidType$Properties.create()
+        .density(2000)
+        .viscosity(10000)
+        .temperature(1000)
+        .descriptionId(`fluid.kubejs.${name}`)
+        .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
+        .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+        // from forge lava type
+        .motionScale(0.0023333333333333335)
+        .canSwim(false).canDrown(false)
+        .pathType(BlockPathTypes.LAVA)
+        .adjacentPathType(null);
+}
+
+// - - - - - - - - - -
+// Fluid Registry
+
+/** Molten Sea Alloy */
+KubeJSFluids.moltenSeaAlloy = FLUIDS.register("molten_sea_alloy")
+    .type(presetHot("molten_sea_alloy")
+        .temperature(1400)
+        .lightLevel(15)
+    )
+    .block(
+        BurningLiquidBlock.createBurning(MapColor.METAL, 15, 10, 6.0)
+    )
+    .commonTag()
+    .bucket()
+    .flowing();
+
+StartupEvents.registry("minecraft:fluid", event => {
+    console.info("Fluid registered!");
+    console.info(FLUIDS["register(net.minecraftforge.eventbus.api.IEventBus)"]);
+    console.info(ForgeEvents.eventBus());
+    console.info(KubeJSFluids.moltenSeaAlloy);
+    FLUIDS["register(net.minecraftforge.eventbus.api.IEventBus)"](ForgeEvents.eventBus());
+});
