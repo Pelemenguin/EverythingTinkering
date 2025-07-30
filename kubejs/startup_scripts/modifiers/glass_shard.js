61864fb96ce6638184c30a551131e7698cf8ae82
Pelemenguin <elepentuin@outlook.com>
2025-07-30 14:11:51 +0800

    Add probability judgement in order to match old descriptions

--- kubejs/startup_scripts/modifiers/glass_shard.js
+++ kubejs/startup_scripts/modifiers/glass_shard.js
@@ -17,6 +17,7 @@
 /* global
     ModifierRegisterer
     AABB
+    JavaMath
 */
 
 /** */
@@ -44,7 +45,9 @@ let GLASS_TYPE_TO_PARTICLE = {
 
 let GLASS_SHARD = ModifierRegisterer.registerModifier("kubejs:glass_shard", ["onBeforeMeleeHit"]);
 GLASS_SHARD.onBeforeMeleeHit((view, lvl, context, damage, baseKnockback, finalKnockback) => {
-    // let ifRepeated = checkIfEntityRepeated(context.getTarget());
+    let chance = (damage - 5.0) * 0.2 * lvl;
+    if (JavaMath.random() >= chance) return finalKnockback;
+    
     let {x, y, z} = context.getTarget();
     let box = AABB.of(
         x - 1,
@@ -69,12 +72,11 @@ GLASS_SHARD.onBeforeMeleeHit((view, lvl, context, damage, baseKnockback, finalKn
         entity.attack(source, damagePerEntity);
     });
     view.getMaterials().forEach(material => {
-        let material_name = material.getId().toString();
+        // let material_name = material.getId().toString();
+        let material_name = material.getVariant().toString();
         if (material_name in GLASS_TYPE_TO_PARTICLE) {
             let particle_block = GLASS_TYPE_TO_PARTICLE[material_name];
             context.attacker.runCommandSilent("particle minecraft:block "+particle_block+" "+x+" "+y+" "+z+" 1 1 1 1 50");
-            // event.server.runCommandSilent("playsound minecraft:block.glass.break player @a "+particle_block+" "+target_x+" "+target_y+" "+target_z+" 10 "+sound_pitch)
-            // context.attacker.playSound("block.glass.break", 5, 0.8);
             context.target.playSound("block.glass.break", 5, 0.8);
         }
     });
