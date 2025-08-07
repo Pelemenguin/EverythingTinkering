e90a8b1899306eecb94850395abb07c160c1c105
Pelemenguin <elepentuin@outlook.com>
2025-08-07 18:13:43 +0800

    Add license declarations for scripts on this branch

--- kubejs/startup_scripts/recipes/material/materialRecipes.js
+++ kubejs/startup_scripts/recipes/material/materialRecipes.js
@@ -2,9 +2,15 @@
 
 /**
  * @fileoverview Part Builder recipes lookup | 部件制造台配方查找
- * 
+ * - - - - -
+ * @copyright Pelemenguin 2025
+ * @license LGPL-3.0-or-later
+ * This file is part of EverythingTinkering.
+ * Full license see file `COPYING.LESSER`
+ * - - - - -
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * - - - - -
  * @author Pelemenguin
- * @license CC-BY-NC-SA-4.0
  */
 
 /* global
