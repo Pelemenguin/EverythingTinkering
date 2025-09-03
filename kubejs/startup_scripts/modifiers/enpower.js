8dc179ea602c3b81a6037eaefe671c5e840760e5
Pelemenguin <elepentuin@outlook.com>
2025-09-03 17:47:52 +0800

    Rename `enpower` to `empower`

--- kubejs/startup_scripts/modifiers/enpower.js
+++ kubejs/startup_scripts/modifiers/enpower.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
  
 /**
- * @fileoverview Enpower | 充能
+ * @fileoverview Empower | 充能
  * - - - - -
  * Consumes Redstone Dust to speed up a projectile.  
  * 消耗红石粉加速弹射物。
@@ -22,7 +22,7 @@
 */
 
 /**
- * - Checks if a item can be accepted by `enpower`.
+ * - Checks if a item can be accepted by `empower`.
  * - 检查一个物品是否能被`充能`接受。
  * - - - - -
  * @type {(stack: Internal.ItemStack) => boolean}
@@ -38,7 +38,7 @@ let ENPOWER_ACCEPTABLE_PREDICATE = (item) => {
 let ENPOWER_PROJECTILE_SPEED_MODIFIER = 0.05;
 
 // eslint-disable-next-line no-unused-vars
-let ENPOWER = ModifierManager.registerCommonModifier("enpower", "EnpowerModifier", {
+let ENPOWER = ModifierManager.registerCommonModifier("empower", "EmpowerModifier", {
     onProjectileLaunch: (tool, modifier, shooter, ammo, projectile /*, arrow, persistent, isPrimary */) => {
         if (!shooter.isPlayer()) return;
         let found = false;
@@ -53,7 +53,7 @@ let ENPOWER = ModifierManager.registerCommonModifier("enpower", "EnpowerModifier
             found = true;
             if (!shooter.isCreative()) stack.count -= 1;
 
-            projectile.mergeNbt({ForgeData: {IsLaunchedByEnpower: NBT.byteTag(1)}});
+            projectile.mergeNbt({ForgeData: {IsLaunchedByEmpower: NBT.byteTag(1)}});
         });
     },
     __custom__: {
@@ -62,7 +62,7 @@ let ENPOWER = ModifierManager.registerCommonModifier("enpower", "EnpowerModifier
                 if (entity.nbt.contains("inGround") && entity.nbt.get("inGround").asByte == 1) return;
                 /** @type {Internal.CompoundTag} */ let forgeData = entity.getNbt().get("ForgeData");
                 if (forgeData == null) return;
-                /** @type {Internal.ByteTag} */ let shouldAddParticles = forgeData.get("IsLaunchedByEnpower");
+                /** @type {Internal.ByteTag} */ let shouldAddParticles = forgeData.get("IsLaunchedByEmpower");
                 if (shouldAddParticles == null) return;
                 if (shouldAddParticles.asByte != 1) return;
                 entity.getLevel().spawnParticles(new DustParticleOptions(new Vec3f(1, 0, 0), 1), false, entity.x, entity.y, entity.z, 0, 0, 0, 5, 0);
