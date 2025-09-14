a7c9d392e1812bb3f389975645748dba552b91ef
Pelemenguin <elepentuin@outlook.com>
2025-09-14 17:33:20 +0800

    Complete work on `algae` modifier

--- kubejs/startup_scripts/modifiers/algae.js
+++ kubejs/startup_scripts/modifiers/algae.js
@@ -1,15 +1,10 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 /**
- * @fileoverview Algea | 未定
+ * @fileoverview Algea | 海藻
  * - - - - -
- * ## Algea
- * ### Description
- * Chance to replace the rock behind the one you mined with ores.
- * - - - - -
- * ## 未定
- * ### 描述
- *在水中恢复耐久
+ * Recovers durability under water.
+ * 在水下回复耐久
  * - - - - -
  * @copyright Pelemenguin 2025
  * @license LGPL-3.0-or-later
@@ -19,18 +14,26 @@
  * @author Pelemenguin
  */
 
-let ALGAE_REPAIR_CLOCK = 20;
+/**
+ * - Passed tick count every 1 durability point recovers.
+ * - 每1回复耐久值经过的Tick数。
+ */
+let ALGAE_REPAIR_CLOCK = 60;
+
 // eslint-disable-next-line no-unused-vars
 let ALGAE = ModifierManager.registerCommonModifier("algae", "AlgaeModifier", {
-    onInventoryTick: (tool, modifier, world, holder, itemSlot, isSelected, isCorrectSlot, stack) => {
+    onInventoryTick: (tool, _modifier, world, holder, _itemSlot, _isSelected, _isCorrectSlot, _stack) => {
         if (world.isClientSide()) return;
-        if(holder.isInWater()) {
-            if(tool.damage >=0 ) {
-                return;
-            }
+        if (holder.isPlayer() && holder.isCreative()) return;
+        if (holder.isUnderWater()) {
             if(world.getTime() % ALGAE_REPAIR_CLOCK == 0) {
-                ToolDamageUtil.repair(tool,1);
+                ToolDamageUtil.repair(tool, 1);
             }
+        }
+    },
+    __class__: {
+        post: (classCreator) => {
+            classCreator.extends("slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier");
+        }
     }
-
-}});
+});
