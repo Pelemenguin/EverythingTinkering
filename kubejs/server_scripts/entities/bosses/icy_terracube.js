9cb3b870a966f45fd799a3594dd887f353156d0a
Pelemenguin <elepentuin@outlook.com>
2025-10-19 22:34:41 +0800

    Add basic definitions for `Icy Terracube`

---
+++ kubejs/server_scripts/entities/bosses/icy_terracube.js
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+/**
+ * @fileoverview Icy Terracube
+ * - - - - -
+ * @copyright Pelemenguin 2025
+ * @license LGPL-3.0-or-later
+ * This file is part of EverythingTinkering.
+ * Full license see file `COPYING.LESSER`
+ * - - - - -
+ * @author Pelemenguin
+ */
+
+// /* global
+//     EntityJSEvents
+// */
+
+// EntityJSEvents.buildBrainProvider("kubejs:icy_terracube", event => {
+//     event.addMemory("minecraft:attack_target");
+// });
+
+// EntityJSEvents.buildBrain("kubejs:icy_terracube", event => {
+//     event.coreActivity(1, [
+        
+//     ]);
+// });
