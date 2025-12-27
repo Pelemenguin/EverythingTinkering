b9f13449c1791182b0a1f7226106d7352a4b3807
Pelemenguin <elepentuin@outlook.com>
2025-12-28 03:08:09 +0800

    Improve Lapis Lazuli part texture generator

--- texture_generator/material_lapis_lazuli.py
+++ texture_generator/material_lapis_lazuli.py
@@ -68,44 +68,82 @@ palette_golden = [
 processed_palette = convert_palette(palette)
 golden_palette = convert_palette(palette_golden)
 
-random.seed("lapis")
+def getPoint(p1: tuple[int|float, int|float], p2: tuple[int|float, int|float], t: float) -> tuple[float, float]:
+    return (p1[0] * t + p2[0] * (1-t), p1[1] * t + p2[1] * (1-t))
+
+def draw_curve(image: PIL.Image.Image, lu: tuple[int, int], rd: tuple[int, int], start: tuple[int, int] | None = None, end: tuple[int, int] | None = None, seed: int = None) -> bool:
+    random.seed(seed if seed is not None else hash((lu, rd)))
+
+    c1: tuple[int, int] = (random.randint(lu[0], rd[0]), random.randint(lu[1], rd[1]))
+    c2: tuple[int, int] = (random.randint(lu[0], rd[0]), random.randint(lu[1], rd[1]))
+
+    begin = start if start is not None else lu
+    finish = end if end is not None else rd
+
+    applicated = 0
+
+    for step in range(0, 41):
+        t: float = step / 40
+    
+        p1: tuple[float, float] = getPoint(begin, c1, t)
+        p2: tuple[float, float] = getPoint(c1, c2, t)
+        p3: tuple[float, float] = getPoint(c2, finish, t)
+    
+        q1: tuple[float, float] = getPoint(p1, p2, t)
+        q2: tuple[float, float] = getPoint(p2, p3, t)
+    
+        r: tuple[float, float] = getPoint(q1, q2, t)
+        point: tuple[int, int] = (round(r[0]), round(r[1]))
+
+        pixel = image.getpixel(point)
+        if (pixel[3] <= 0): continue
+        if (pixel[0] != pixel[1] or pixel[1] != pixel[2]): continue
+
+        grey: int = pixel[2]
+        color: tuple[int, int, int, int]
+        try:
+            color = golden_palette[grey]
+        except:
+            for g in golden_palette:
+                if g > grey:
+                    color = golden_palette[g]
+                    break
+
+        image.putpixel(point, color)
+        applicated += 1
+
+    return applicated / 40 >= 0.1 or applicated >= (rd[0] - lu[0]) * (rd[1] - lu[1]) * 0.1
+
 def golden_strip(image: PIL.Image.Image) -> PIL.Image.Image:
-    random.seed("lapis" + str(image.tobytes()))
-    for _ in range(4):
-        controls = [(random.randint(-4, 4)*image.width, random.randint(-8, 8)*image.height) for _ in range(2)]
-        # controls = [(0, 8), (4, 12)]
-        copied = image.copy()
-        # for p in controls:
-            # copied.putpixel(p, (255, 0, 0, 255))
-        processed = sorted(golden_palette, reverse=True)
-        p = 0
-        while p <= 1:
-            q1 = (p*controls[0][0], p*controls[0][1])
-            q2 = (p*controls[1][0]+(1-p)*controls[0][0], p*controls[1][1]+(1-p)*controls[0][1])
-            # q3 = (p*controls[2][0]+(1-p)*controls[1][0], p*controls[2][0]+(1-p)*controls[1][1])
-            q3 = (p*15+(1-p)*controls[1][0], p*15+(1-p)*controls[1][1])
-            r1 = (p*q2[0]-(1-p)*(q1[0]), p*q2[1]-(1-p)*(q1[1]))
-            r2 = (p*q3[0]-(1-p)*(q2[0]), p*q3[1]-(1-p)*(q2[1]))
-            # r3 = (p*q4[0]-(1-p)*(q3[0]), p*q4[1]-(1-p)*(q3[1]))
-            cur = (p*r2[0]-(1-p)*(r1[0]), p*r2[1]-(1-p)*(r1[1]))
-            # s2 = (p*r3[0]-(1-p)*(r2[0]), p*r3[1]-(1-p)*(r2[1]))
-            # cur = (p*s2[0]-(1-p)*(s1[0]), p*s2[1]-(1-p)*(s1[1]))
-            p += 0.001
-            # if not (0<=cur[0]<=15 and 0<=cur[1]<=15):
-                # continue
-            # print(cur)
-            cur = (round(cur[0])%image.width, round(cur[1])%image.height)
-            # print(cur)
-            if copied.getpixel(cur) == (0, 0, 0, 0):
-                continue
-            try:
-                copied.putpixel(cur, golden_palette[image.getpixel(cur)[1]])
-                # copied.putpixel((cur[0], cur[1]+1 if cur[1]<=14 else cur), tuple(int(0.5*i) for i in golden_palette[image.getpixel(cur)[1]]))
-            except:
-                for k in processed:
-                    if copied.getpixel(cur)[1] >= k:
-                        copied.putpixel(cur, golden_palette[k])
-    return copied
+    leftmost: int = 127
+    rightmost: int = 0
+    upmost: int = 127
+    downmost: int = 0
+
+    for x in range(image.width):
+        for y in range(image.height):
+            if (image.getpixel((x,y))[3] != 0):
+                leftmost = min(x, leftmost)
+                rightmost = max(x, rightmost)
+                upmost = min(y, upmost)
+                downmost = max(y, downmost)
+
+    print(f"({leftmost}, {upmost}) -> ({rightmost}, {downmost})")
+
+    effective = draw_curve(image, (leftmost, upmost), (rightmost, downmost))
+
+    if effective: return image
+
+    seed = hash((leftmost, upmost, rightmost, downmost))
+    attempts: int = 0
+    while attempts < 3 and not effective:
+        seed = hash((seed, attempts))
+        effective = draw_curve(image, (leftmost, upmost), (rightmost, downmost),
+            (leftmost, random.randint((upmost + downmost) // 2, downmost)), (rightmost, random.randint((upmost + downmost) // 2, downmost)), seed
+        )
+        attempts += 1
+
+    return image
 
 generator = TextureGenerator(parts)
 generator.add_function(golden_strip, "golden", 1)
