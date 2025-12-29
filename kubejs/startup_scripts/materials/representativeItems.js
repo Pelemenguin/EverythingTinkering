3280994d027b9fc6a14d1b0f8c2556b17cfde058
Pelemenguin <elepentuin@outlook.com>
2025-12-29 18:10:08 +0800

    Rearrange materials

--- kubejs/startup_scripts/materials/representativeItems.js
+++ kubejs/startup_scripts/materials/representativeItems.js
@@ -29,136 +29,146 @@ const HIDDEN_MATERIAL = {};
  */
 let reprItems = {
 
-    // Tier 1
+    // ========== Main Line / No Line ========== //
 
-    // Non-Metal
+    // ---------- Herbal ---------- //
+
+    // Common
     "tconstruct:wood": "minecraft:oak_log",
     "tconstruct:bamboo": "minecraft:bamboo",
     "tconstruct:cactus": "minecraft:cactus",
-    "tconstruct:rock": "minecraft:stone",
-    "kubejs:soil": "minecraft:dirt",
-    "kubejs:terracotta": "minecraft:terracotta",
-    "kubejs:brick": "minecraft:brick",
-    "tconstruct:feather": "minecraft:feather",
     "tconstruct:leaves": "minecraft:oak_leaves",
-    "tconstruct:paper": "minecraft:paper",
-    "tconstruct:wool": "minecraft:white_wool",
+    "kubejs:sugar_cane": "minecraft:sugar_cane",
+    "kubejs:kelp": "minecraft:kelp",
+    "tconstruct:slimewood": "tconstruct:greenheart_log",
+    "tconstruct:blazewood": "tconstruct:blazewood",
 
-    // Minerals
+    // Vine-Like
+    "tconstruct:vine": "minecraft:vine",
+    "tconstruct:skyslime_vine": "tconstruct:sky_slime_vine",
+    "tconstruct:enderslime_vine": "tconstruct:ender_slime_vine",
+    "tconstruct:twisting_vine": "minecraft:twisting_vines",
+    "tconstruct:weeping_vine": "minecraft:weeping_vines",
+
+    // End
+    "tconstruct:chorus": "minecraft:popped_chorus_fruit",
+
+    // ---------- Geological---------- //
+
+    // Ground
+    "kubejs:soil": "minecraft:dirt",
+    "tconstruct:rock": "minecraft:stone",
+    "kubejs:terracotta": "minecraft:terracotta",
+    "tconstruct:ice": "minecraft:ice",
+
+    // Relatively Rare
+    "tconstruct:flint": "minecraft:flint",
+    "tconstruct:prismarine": "minecraft:prismarine_shard",
+
+    // Nether
+    "kubejs:magma": "minecraft:magma_block",
+    "tconstruct:obsidian": "minecraft:obsidian", // Not very "Nether", but put it after Magma Block
+    "kubejs:crying_obsidian": "minecraft:crying_obsidian",
+    "tconstruct:glowstone": "minecraft:glowstone_dust",
+
+    // Common Ore
     "kubejs:coal": "minecraft:coal",
     "kubejs:lapis_lazuli": "minecraft:lapis_lazuli",
     "kubejs:redstone": "minecraft:redstone",
 
-    // Other
-    "tconstruct:flint": "minecraft:flint",
-    "tconstruct:bone": "minecraft:bone",
-    "kubejs:magma": "minecraft:magma_block",
-    "tconstruct:chorus": "minecraft:popped_chorus_fruit",
-
-    // Metal
+    // Ingots
     "tconstruct:copper": "minecraft:copper_ingot",
-    "kubejs:andesite_alloy": "create:andesite_alloy",
-
-    // Part-Specified Materials
-    "kubejs:sugar_cane": "minecraft:sugar_cane",
-    "tconstruct:string": "minecraft:string",
-    "tconstruct:leather": "minecraft:leather",
-    "tconstruct:vine": "minecraft:vine",
-    "kubejs:kelp": "minecraft:kelp",
-    "tconstruct:ice": "minecraft:ice",
-    "kubejs:phantom_membrane": "minecraft:phantom_membrane",
-
-    // Technology
-    "kubejs:andesite_alloy": "create:andesite_alloy",
-
-    // Adventure
-    "kubejs:scrapped_tinker_metal": "kubejs:scrapped_tinker_metal",
-
-    // Tier 2
-
-    // Non-Metal Materials
-    "tconstruct:treated_wood": "thermal:creosote_bucket",
-    "tconstruct:seared_stone": "tconstruct:seared_brick",
-    "tconstruct:scorched_stone": "tconstruct:scorched_brick",
-    "tconstruct:slimewood": "tconstruct:greenheart_log",
-    "tconstruct:prismarine": "minecraft:prismarine_shard",
-
-    // Metals & Minerals
     "tconstruct:iron": "minecraft:iron_ingot",
     "tconstruct:lead": "thermal:lead_ingot",
     "tconstruct:silver": "thermal:silver_ingot",
     "tconstruct:gold": "minecraft:gold_ingot",
-    "tconstruct:amethyst": "minecraft:amethyst_shard",
-    "tconstruct:earthslime": "tconstruct:earth_slime_crystal",
-    "tconstruct:skyslime": "tconstruct:sky_slime_crystal",
-    "tconstruct:slimeball": "minecraft:slime_ball",
-
-    // Other
-    "tconstruct:gunpowder": "minecraft:gunpowder",
-    "tconstruct:ender_pearl": "minecraft:ender_pearl",
-    "tconstruct:blaze": "minecraft:blaze_rod",
-    "tconstruct:venombone": "tconstruct:venombone",
-    "tconstruct:necrotic_bone": "tconstruct:necrotic_bone",
-    "tconstruct:whitestone": "minecraft:end_stone",
-    "tconstruct:skyslime_vine": "tconstruct:sky_slime_vine",
-    "tconstruct:twisting_vine": "minecraft:twisting_vines",
-    "tconstruct:weeping_vine": "minecraft:weeping_vines",
-    "tconstruct:glass": "minecraft:glass",
-    "tconstruct:slimeskin": "tconstruct:earth_slime_bucket",
-
-    "kubejs:sea_alloy": HIDDEN_MATERIAL, // Not prepared yet
-
-    // ========== Tier 3 ========== //
-
-    // Non-Metal
-    "tconstruct:magma": "minecraft:magma_cream",
-    "tconstruct:quartz": "minecraft:quartz",
-    "tconstruct:obsidian": "minecraft:obsidian",
-    "kubejs:crying_obsidian": "minecraft:crying_obsidian",
-    "tconstruct:glowstone": "minecraft:glowstone_dust",
-    "tconstruct:ichor": "tconstruct:ichor_slime_crystal",
-
-    // Metal
     "tconstruct:cobalt": "tconstruct:cobalt_ingot",
-    "tconstruct:magnetite": "tconstruct:steel_shard",
 
-    // Alloys
+    // Alloy Ingots // Not very `geological`, but place them after Ingots
     "tconstruct:steel": "tconstruct:steel_ingot",
     "tconstruct:bronze": "thermal:bronze_ingot",
     "tconstruct:constantan": "thermal:constantan_ingot",
     "tconstruct:invar": "thermal:invar_ingot",
     "tconstruct:electrum": "thermal:electrum_ingot",
     "tconstruct:rose_gold": "tconstruct:rose_gold_ingot",
-
-    // Imaginary Alloys
     "tconstruct:amethyst_bronze": "tconstruct:amethyst_bronze_ingot",
     "tconstruct:pig_iron": "tconstruct:pig_iron_ingot",
-    "tconstruct:slimesteel": "tconstruct:slimesteel_ingot",
     "tconstruct:pewter": "tconstruct:molten_pewter_bucket", // Pewter has no ingots, put it also here
-
-    // Composite Materials
-    "tconstruct:nahuatl": "tconstruct:nahuatl",
     "tconstruct:plated_slimewood": "create:brass_ingot",
-
-    // Part-Specified Materials
-    "tconstruct:darkthread": "minecraft:obsidian",
-    "tconstruct:ichorskin": "tconstruct:ichor_bucket",
-
-    // Tier 4
-    "tconstruct:blazewood": "tconstruct:blazewood",
-    "tconstruct:shulker": "minecraft:shulker_shell",
     "tconstruct:hepatizon": "tconstruct:hepatizon_ingot",
     "tconstruct:manyullyn": "tconstruct:manyullyn_ingot",
-    "tconstruct:knightmetal": "tconstruct:knightmetal_ingot",
-    "tconstruct:knightly": "tconstruct:knightmetal_shard",
+
+    "tconstruct:slimesteel": "tconstruct:slimesteel_ingot",
     "tconstruct:cinderslime": "tconstruct:cinderslime_ingot",
-    "tconstruct:enderslime": "tconstruct:ender_slime_crystal",
     "tconstruct:queens_slime": "tconstruct:queens_slime_ingot",
-    "tconstruct:end_rod": "minecraft:end_rod",
-    "tconstruct:blazing_bone": "tconstruct:blazing_bone",
-    "tconstruct:dragon_scale": "tconstruct:dragon_scale",
+    "tconstruct:knightmetal": "tconstruct:knightmetal_ingot",
+
+    // Crystals
+    "tconstruct:quartz": "minecraft:quartz",
+    "tconstruct:amethyst": "minecraft:amethyst_shard",
+    "tconstruct:earthslime": "tconstruct:earth_slime_crystal",
+    "tconstruct:skyslime": "tconstruct:sky_slime_crystal",
+    "tconstruct:enderslime": "tconstruct:ender_slime_crystal",
+    "tconstruct:ichor": "tconstruct:ichor_slime_crystal",
+
+    // End
+    "tconstruct:whitestone": "minecraft:end_stone",
+
+    // Other Metal
+    "tconstruct:magnetite": "tconstruct:steel_shard",
+    "tconstruct:knightly": "tconstruct:knightmetal_shard",
     "tconstruct:ancient_hide": "minecraft:netherite_scrap",
-    "tconstruct:enderslime_vine": "tconstruct:ender_slime_vine",
+
+    // ---------- Building ---------- //
+
+    "tconstruct:glass": "minecraft:glass",
+    "kubejs:brick": "minecraft:brick",
+    "tconstruct:seared_stone": "tconstruct:seared_brick",
+    "tconstruct:scorched_stone": "tconstruct:scorched_brick",
+    "tconstruct:nahuatl": "tconstruct:nahuatl",
+    "tconstruct:end_rod": "minecraft:end_rod",
+
+    // ---------- Loot ---------- //
+
+    // Passive
+    "tconstruct:feather": "minecraft:feather",
+    "tconstruct:wool": "minecraft:white_wool",
+    "tconstruct:leather": "minecraft:leather",
+    "tconstruct:slimeskin": "tconstruct:earth_slime_bucket",
+    "tconstruct:ichorskin": "tconstruct:ichor_bucket",
+
+    // Aggresive
+    "tconstruct:bone": "minecraft:bone",
+    "tconstruct:venombone": "tconstruct:venombone",
+    "tconstruct:necrotic_bone": "tconstruct:necrotic_bone",
+    "tconstruct:blazing_bone": "tconstruct:blazing_bone",
+    "tconstruct:string": "minecraft:string",
+    "tconstruct:gunpowder": "minecraft:gunpowder",
+    "tconstruct:ender_pearl": "minecraft:ender_pearl",
+    "tconstruct:slimeball": "minecraft:slime_ball",
+    "tconstruct:magma": "minecraft:magma_cream",
+    "tconstruct:blaze": "minecraft:blaze_rod",
+    "kubejs:phantom_membrane": "minecraft:phantom_membrane",
+    "tconstruct:shulker": "minecraft:shulker_shell",
+    "tconstruct:dragon_scale": "tconstruct:dragon_scale",
+
+    // ---------- Miscalleous ---------- //
+
+    "tconstruct:paper": "minecraft:paper",
+    "tconstruct:darkthread": "minecraft:obsidian",
+
+    // ========== Technology Line ========== //
+
+    "tconstruct:treated_wood": "thermal:creosote_bucket",
+    "kubejs:andesite_alloy": "create:andesite_alloy",
+
+    // ========== Adventure Line ========== //
+
+    "kubejs:scrapped_tinker_metal": "kubejs:scrapped_tinker_metal",
+
+    // ========== Hidden ========== //
+
+    "kubejs:sea_alloy": HIDDEN_MATERIAL, // Not prepared yet
 
 };
 
