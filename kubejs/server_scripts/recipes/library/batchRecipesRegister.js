f650878430e5d7d866d3a4e3cb447c8be8aec80d
Pelemenguin <elepentuin@outlook.com>
2026-01-31 12:32:18 +0800

    Move batch recipes declaring to startup_scripts

---
+++ kubejs/server_scripts/recipes/library/batchRecipesRegister.js
@@ -0,0 +1,92 @@
+// priority: 32767
+
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+/**
+ * @fileoverview Batch Recipe Registration | 批量配方注册
+ * - - - - -
+ * @copyright Pelemenguin 2025
+ * @license LGPL-3.0-or-later
+ * This file is part of EverythingTinkering.
+ * Full license see file `COPYING.LESSER`
+ * - - - - -
+ * @author Pelemenguin
+ */
+
+/* global
+    global: writable
+    ServerEvents
+    $MaterialIngredient
+    console
+    InputItem
+    OutputItem
+    ToolPartItem
+    $MaterialStatsId
+ */
+
+const BatchMaterialRecipes = global.BatchMaterialRecipes;
+
+ServerEvents.recipes(event => {
+
+    console.info(`Registering batch material recipes...`);
+    console.info(`Available tool parts: ${global.CustomUtils.Tinker.TOOL_PARTS.map(part => part.getId()).join(", ")}`);
+    
+    const REPAIR_KIT_STAT_ID = $MaterialStatsId.tryParse("tconstruct:repair_kit");
+
+    // Deploying
+    for (let recipeId in BatchMaterialRecipes.Deploying.ALL) {
+        let entry = BatchMaterialRecipes.Deploying.ALL[recipeId];
+        console.info(`Registering deploying batch recipe: ${recipeId} (${entry.inputMaterial} + ${entry.usingItem} -> ${entry.outputMaterial})`);
+        for (let part of global.CustomUtils.Tinker.TOOL_PARTS) {
+            if (!(part instanceof ToolPartItem)) {
+                if (!entry.partStatTypes.contains(REPAIR_KIT_STAT_ID)) {
+                    console.info(`Skipping ${part.getId()} as for stat type tconstruct:repair_kit`);
+                    continue;
+                }
+            } else {
+                if (!entry.partStatTypes.contains(part.getStatType())) {
+                    console.info(`Skipping ${part.getId()} for stat type ${part.getStatType()}`);
+                    continue;
+                }
+            }
+
+            let inputPart = InputItem.of($MaterialIngredient["of(net.minecraft.world.level.ItemLike,slimeknights.tconstruct.library.materials.definition.MaterialVariantId)"](part, entry.inputMaterial), 1);
+
+            event.getRecipes().create.deploying(
+                [OutputItem.of(part.withMaterialForDisplay(entry.outputMaterial))],
+                [inputPart, InputItem.of(entry.usingItem, 1)]
+            ).id(recipeId.toString() + "/" + part.getId().replace(":", "/"));
+        }
+    }
+
+    // Sequenced Assembly
+    for (let recipeId in BatchMaterialRecipes.SequencedAssembly.ALL) {
+        let entry = BatchMaterialRecipes.SequencedAssembly.ALL[recipeId];
+        console.info(`Registering sequenced assembly batch recipe: ${recipeId} (${entry.inputMaterial} -> ${entry.outputMaterial})`);
+        for (let part of global.CustomUtils.Tinker.TOOL_PARTS) {
+            if (!(part instanceof ToolPartItem)) {
+                if (!entry.partStatTypes.contains(REPAIR_KIT_STAT_ID)) {
+                    console.info(`Skipping ${part.getId()} as for stat type tconstruct:repair_kit`);
+                    continue;
+                }
+            } else {
+                if (!entry.partStatTypes.contains(part.getStatType())) {
+                    console.info(`Skipping ${part.getId()} for stat type ${part.getStatType()}`);
+                    continue;
+                }
+            }
+
+            let inputPart = InputItem.of($MaterialIngredient["of(net.minecraft.world.level.ItemLike,slimeknights.tconstruct.library.materials.definition.MaterialVariantId)"](part, entry.inputMaterial), 1);
+            let transitionalItem = part.withMaterialForDisplay(entry.transitionalMaterial);
+
+            event.getRecipes().create.sequenced_assembly(
+                [OutputItem.of(part.withMaterialForDisplay(entry.outputMaterial))],
+                inputPart,
+                entry.sequence(event, transitionalItem, part),
+                part.withMaterialForDisplay(entry.transitionalMaterial),
+                entry.loops
+            ).id(recipeId.toString() + "/" + part.getId().replace(":", "/"));
+        }
+    }
+
+});
--- kubejs/startup_scripts/registries/recipes/library/batchRecipes.js
+++ kubejs/startup_scripts/registries/recipes/library/batchRecipes.js
@@ -15,12 +15,6 @@
 
 /* global
     global: writable
-    ServerEvents
-    $MaterialIngredient
-    console
-    InputItem
-    OutputItem
-    ToolPartItem
     $HashSet
     $MaterialStatsId
     Utils
@@ -219,70 +213,5 @@ BatchMaterialRecipes.SequencedAssembly = {
         BatchMaterialRecipes.SequencedAssembly.CACHE.get(materialId).push(recipe);
     }
 };
-
-ServerEvents.recipes(event => {
-
-    console.info(`Registering batch material recipes...`);
-    console.info(`Available tool parts: ${global.CustomUtils.Tinker.TOOL_PARTS.map(part => part.getId()).join(", ")}`);
-    
-    const REPAIR_KIT_STAT_ID = $MaterialStatsId.tryParse("tconstruct:repair_kit");
-
-    // Deploying
-    for (let recipeId in BatchMaterialRecipes.Deploying.ALL) {
-        let entry = BatchMaterialRecipes.Deploying.ALL[recipeId];
-        console.info(`Registering deploying batch recipe: ${recipeId} (${entry.inputMaterial} + ${entry.usingItem} -> ${entry.outputMaterial})`);
-        for (let part of global.CustomUtils.Tinker.TOOL_PARTS) {
-            if (!(part instanceof ToolPartItem)) {
-                if (!entry.partStatTypes.contains(REPAIR_KIT_STAT_ID)) {
-                    console.info(`Skipping ${part.getId()} as for stat type tconstruct:repair_kit`);
-                    continue;
-                }
-            } else {
-                if (!entry.partStatTypes.contains(part.getStatType())) {
-                    console.info(`Skipping ${part.getId()} for stat type ${part.getStatType()}`);
-                    continue;
-                }
-            }
-
-            let inputPart = InputItem.of($MaterialIngredient["of(net.minecraft.world.level.ItemLike,slimeknights.tconstruct.library.materials.definition.MaterialVariantId)"](part, entry.inputMaterial), 1);
-
-            event.getRecipes().create.deploying(
-                [OutputItem.of(part.withMaterialForDisplay(entry.outputMaterial))],
-                [inputPart, InputItem.of(entry.usingItem, 1)]
-            ).id(recipeId.toString() + "/" + part.getId().replace(":", "/"));
-        }
-    }
-
-    // Sequenced Assembly
-    for (let recipeId in BatchMaterialRecipes.SequencedAssembly.ALL) {
-        let entry = BatchMaterialRecipes.SequencedAssembly.ALL[recipeId];
-        console.info(`Registering sequenced assembly batch recipe: ${recipeId} (${entry.inputMaterial} -> ${entry.outputMaterial})`);
-        for (let part of global.CustomUtils.Tinker.TOOL_PARTS) {
-            if (!(part instanceof ToolPartItem)) {
-                if (!entry.partStatTypes.contains(REPAIR_KIT_STAT_ID)) {
-                    console.info(`Skipping ${part.getId()} as for stat type tconstruct:repair_kit`);
-                    continue;
-                }
-            } else {
-                if (!entry.partStatTypes.contains(part.getStatType())) {
-                    console.info(`Skipping ${part.getId()} for stat type ${part.getStatType()}`);
-                    continue;
-                }
-            }
-
-            let inputPart = InputItem.of($MaterialIngredient["of(net.minecraft.world.level.ItemLike,slimeknights.tconstruct.library.materials.definition.MaterialVariantId)"](part, entry.inputMaterial), 1);
-            let transitionalItem = part.withMaterialForDisplay(entry.transitionalMaterial);
-
-            event.getRecipes().create.sequenced_assembly(
-                [OutputItem.of(part.withMaterialForDisplay(entry.outputMaterial))],
-                inputPart,
-                entry.sequence(event, transitionalItem, part),
-                part.withMaterialForDisplay(entry.transitionalMaterial),
-                entry.loops
-            ).id(recipeId.toString() + "/" + part.getId().replace(":", "/"));
-        }
-    }
-
-});
 
 global.BatchMaterialRecipes = BatchMaterialRecipes;
